50cee4339ed99479c6b428c8844d299cbd3da63a
erishen <lsun@yinnut.com>
2017-03-26 15:37:28 +0800

    add 3 line container
    Summary: None
    
    Test: OK

--- src/containers/InterviewPage.js
+++ src/containers/InterviewPage.js
@@ -10,15 +10,13 @@ class InterviewPage extends Component {
     render() {
         return (
             <div id="interview_div">
-                <button className="btn btn-warning" type="submit">Button</button>
-                <div id="demo"></div>
-
-                <div id="top_div"></div>
-                <div id="bottom_left_div"></div>
-                <div id="bottom_right_div"></div>
-                <div id="bottom_clear_div"></div>
-
-                <div id="middle_div"></div>
+                <div id="head">我是头部</div>
+                <div id="main">
+                    <div id="left">我是左边,宽:200px</div>
+                    <div id="right">我是右边,宽:200px</div>
+                    <div id="mid">我是中间,宽自适应</div>
+                </div>
+                <div id="foot">我是底部</div>
             </div>
         );
     }
